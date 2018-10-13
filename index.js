9cf27d4e1e5fb955c6d2ac36019c1ab2300d9a2b
Ryan Stelly <ryan.stelly@live.com>
2018-10-13 15:11:24 -0500

    first pass at table results

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const axios = require('axios');
 const { RegistryClient } = require('package-metadata');
 const octokit = require('@octokit/rest')();
 
+const Table = require('cli-table');
+
 const { promisify } = require('util');
 const fs = require('fs');
 const readFileAsync = promisify(fs.readFile);
@@ -11,6 +13,10 @@ const readFileAsync = promisify(fs.readFile);
 // ["github.com/axios/axios.git", "/", "axios", "axios"]
 // [, , owner, repo]
 const repositoryPattern = /github\.com(\/|:)(.*)\/(.*)\.git/;
+
+function chunkString(str, length) {
+  return str.match(new RegExp('.{1,' + length + '}', 'g'));
+}
 
 function error(message) {
   console.error(message);
@@ -33,7 +39,7 @@ function modelDependencies(json, dependencyType) {
 }
 
 async function getRepositoryForPackage(packageName) {
-  const metadata = await RegistryClient.getMetadata('axios', {
+  const metadata = await RegistryClient.getMetadata(packageName, {
     fullMetadata: true,
   });
 
@@ -56,25 +62,68 @@ async function getDependencyWithRepo(dependency) {
   };
 }
 
+async function getIssuesForDependency(dependency) {
+  if (!dependency.repository) return { ...dependency, issues: [] };
+
+  const {
+    repository: { owner, repo },
+  } = dependency;
+  const { data } = await octokit.issues.getForRepo({
+    owner,
+    repo,
+    // owner: 'facebook',
+    // repo: 'react',
+    labels: 'Good first issue',
+  });
+
+  return {
+    ...dependency,
+    issues: data.map(issue => ({
+      title: issue.title,
+      link: issue.html_url,
+    })),
+  };
+}
+
+function printIssueTable(dependency) {
+  console.log(`\n${dependency.name}`);
+  if (!dependency.issues.length) {
+    console.log('No "Good First Task" issues :(');
+    return;
+  }
+
+  const table = new Table({
+    head: ['Issue Label', 'Github Issue'],
+    colWidths: [50, 60],
+  });
+  const rows = dependency.issues.map(({ title, link }) => {
+    const formattedTitle = chunkString(title, 47).join('\n');
+    return [formattedTitle, link];
+  });
+  table.push(...rows);
+  console.log(table.toString());
+}
+
 async function run() {
   const contents = await getContents();
 
-  // const json = JSON.parse(contents);
+  const json = JSON.parse(contents);
 
-  // const dependencies = [
-  //   ...modelDependencies(json, 'dependencies'),
-  //   ...modelDependencies(json, 'devDependencies'),
-  //   ...modelDependencies(json, 'peerDependnecies'),
-  // ];
+  let dependencies = [
+    ...modelDependencies(json, 'dependencies'),
+    ...modelDependencies(json, 'devDependencies'),
+    ...modelDependencies(json, 'peerDependnecies'),
+  ];
 
-  // const foo = await Promise.all(dependencies.map(getDependencyWithRepo));
+  const dependenciesWithRepos = await Promise.all(
+    dependencies.map(getDependencyWithRepo)
+  );
 
-  const { data } = await octokit.issues.getForRepo({
-    owner: 'facebook',
-    repo: 'react-native',
-    labels: ['Good first issue'],
-  });
-  console.log(data[0]);
+  dependenciesWithIssues = await Promise.all(
+    dependenciesWithRepos.map(getIssuesForDependency)
+  );
+
+  dependenciesWithIssues.forEach(printIssueTable);
 }
 
 run();
